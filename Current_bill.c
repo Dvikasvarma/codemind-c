686eae0e7f54033b1e9c295f9db4b61ade8ea304
Dvikasvarma <147792362+Dvikasvarma@users.noreply.github.com>
2023-11-07 05:41:32 +0530

    #include<stdio.h> 	int main() 	{ 		int u; 		float tb,uc,b; 		scanf("%d",&u); 		if(u<200) 		uc=1.20; 		else if(u>=200 && u<400) 		uc=1.50; 		else if(u>=400 && u<600) 		uc=1.80; 		else if(u>=600) 		uc=2.00;
    		b=u*uc;
    		
    		if(b>400)
    		tb=	b+(b*0.15);
    		else
    		tb=b+100;
    		printf("%.2f",tb);
    		
    	}

---
+++ Current_bill.c
@@ -0,0 +1,24 @@
+	#include<stdio.h>
+	int main()
+	{
+		int u;
+		float tb,uc,b;
+		scanf("%d",&u);
+		if(u<200)
+		uc=1.20;
+		else if(u>=200 && u<400)
+		uc=1.50;
+		else if(u>=400 && u<600)
+		uc=1.80;
+		else if(u>=600)
+		uc=2.00;
+		
+		b=u*uc;
+		
+		if(b>400)
+		tb=	b+(b*0.15);
+		else
+		tb=b+100;
+		printf("%.2f",tb);
+		
+	}
