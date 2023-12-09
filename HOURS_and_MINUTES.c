4a9679a67946f2b61b9f7eeb0cc3ff180b6f1509
Dvikasvarma <147792362+Dvikasvarma@users.noreply.github.com>
2023-12-09 11:41:26 +0530

    #include <stdio.h> void hours() {     int n, h, m;     scanf("%d", &n);
        h = n / 60;
        m = n % 60;
    
        printf("%d Hour(s) %d Minute(s)
    ", h, m);
    }
    int main()
    {
    
       hours();
    }

---
+++ HOURS_and_MINUTES.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void hours()
+{
+    int n, h, m;
+    scanf("%d", &n);
+
+    h = n / 60;
+    m = n % 60;
+
+    printf("%d Hour(s) %d Minute(s)
+", h, m);
+}
+int main()
+{
+
+   hours();
+}
