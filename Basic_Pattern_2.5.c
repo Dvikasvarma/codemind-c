e453c22e8ad452b1a9e9cbaed46403ff0ffc0b6d
Dvikasvarma <147792362+Dvikasvarma@users.noreply.github.com>
2023-12-11 19:45:54 +0530

    #include<stdio.h> int main() {     int n,i,j; scanf("%d",&n); for(i=64+n;i>=65;i--) {     for(j=i;j<=64+n;j++) {     printf("%c ",i);
    }      
    printf("
    ");
        
    }
        
    }

---
+++ Basic_Pattern_2.5.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+scanf("%d",&n);  
+for(i=64+n;i>=65;i--)  
+{
+    for(j=i;j<=64+n;j++)        
+{
+    printf("%c ",i);      
+    
+}      
+printf("
+");
+    
+}
+    
+}
