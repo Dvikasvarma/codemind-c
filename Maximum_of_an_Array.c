7cc408bd9048f552ed7c2220444290a0737df5e6
Dvikasvarma <147792362+Dvikasvarma@users.noreply.github.com>
2023-12-16 09:51:54 +0530

    #include<stdio.h> int main() { 	int n,i,max; 	int a[n]; 	scanf("%d",&n); 	for(i=0;i<n;i++) 	{ 		scanf("%d",&a[i]); 	} 	max=a[0]; 	for(i=0;i<n;i++) 	{ 		if(max<a[i]) 		max=a[i]; 	} 	printf("%d",max); }

---
+++ Maximum_of_an_Array.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,max;
+	int a[n];
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	max=a[0];
+	for(i=0;i<n;i++)
+	{
+		if(max<a[i])
+		max=a[i];
+	}
+	printf("%d",max);
+}
